388fed9b0e63682a0004379745241a8d869e5dfe
Antonstiko <a.hunsalzer@web.de>
2025-04-15 12:00:59 +0200

    everything

---
+++ src/main/java/de/antonstiko/languageloader/LanguageLoader.java
@@ -0,0 +1,27 @@
+package de.antonstiko.consolegame;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import com.google.gson.Gson;
+
+public class LanguageLoader {
+    private static Map<String, String> translations;
+
+    public static void loadLanguage(String langCode) throws IOException {
+        // Benutze den ClassLoader, um die Datei aus den Ressourcen zu laden
+        String path = "/languages/" + langCode + ".json";
+        InputStream inputStream = LanguageLoader.class.getResourceAsStream(path);
+
+        if (inputStream == null) {
+            throw new IOException("Die Sprachdatei existiert nicht: " + langCode);
+        }
+
+        String json = new String(inputStream.readAllBytes());
+        translations = new Gson().fromJson(json, Map.class);
+    }
+
+
+    public static String translate(String key) {
+        return translations.getOrDefault(key, key);
+    }
+}
